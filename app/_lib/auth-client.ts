6c068f29436db79a5081fc4c60b0491e44885a82
Daviafonso88 <daviafonso5894@gmail.com>
2026-03-08 17:05:26 -0300

    refactor: remove Google sign-in integration from auth-client to simplify authentication setup

--- app/_lib/auth-client.ts
+++ app/_lib/auth-client.ts
@@ -1,10 +1,5 @@
-import { google } from "better-auth";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
-
-authClient.signIn.social({
-  provider: "google",
 });
